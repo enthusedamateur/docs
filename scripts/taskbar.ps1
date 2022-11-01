07a99b1d294c1619483b615b81d8163e736bc228
enthusedamateur <56199540+enthusedamateur@users.noreply.github.com>
2022-11-01 22:31:02 +0000

    Create taskbar.ps1
    New testing script with comments for next steps

---
+++ scripts/taskbar.ps1
@@ -0,0 +1,19 @@
+# Unpin Microsoft Store app from taskbar
+ $msstore = "Microsoft Store"
+ ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $msstore}).Verbs() | ?{$_.Name.replace('&','') -match 'Unpin from taskbar'} | %{$_.DoIt(); $exec = $true}
+
+# Unpin Default Mail app from taskbar
+ $mail = "Mail"
+ ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $mail}).Verbs() | ?{$_.Name.replace('&','') -match 'Unpin from taskbar'} | %{$_.DoIt(); $exec = $true}
+
+# Turn off search box on taskbar. User can renable via GUI. 
+# New-Item -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search' -name 'SearchboxTaskbarMode' -type 4 -value 0
+ 
+# Diable Cortana button on taskbar
+
+
+# Disable News and Weather on taskbar
+ Set-ItemProperty -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds' -name 'ShellFeedsTaskbarViewMode' -type 4 -value 0
+
+
+# Pin MS Apps to taskbar
