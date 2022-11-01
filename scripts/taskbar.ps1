58992ddf767526c8c17a03bbbca936eb479ba438
enthusedamateur <56199540+enthusedamateur@users.noreply.github.com>
2022-11-01 22:31:38 +0000

    Uncommented a line

--- scripts/taskbar.ps1
+++ scripts/taskbar.ps1
@@ -7,7 +7,7 @@
  ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $mail}).Verbs() | ?{$_.Name.replace('&','') -match 'Unpin from taskbar'} | %{$_.DoIt(); $exec = $true}
 
 # Turn off search box on taskbar. User can renable via GUI. 
-# New-Item -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search' -name 'SearchboxTaskbarMode' -type 4 -value 0
+ New-Item -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search' -name 'SearchboxTaskbarMode' -type 4 -value 0
  
 # Diable Cortana button on taskbar
 
