b53decea33428a73dcd738efc5d09e1e7ff6a4d4
enthusedamateur <56199540+enthusedamateur@users.noreply.github.com>
2022-12-16 19:31:05 +0000

    Added global configs and comments

--- indev/git_install.sh
+++ indev/git_install.sh
@@ -6,3 +6,16 @@ git config --global init.defaultBranch main
 git config --global pull.rebase false
 git config --global pull.rebase true 
 git config --global pull.ff only
+git config --global user.name "Your name here"
+git config --global user.email "your_email@example.com"
+git config --global color.ui true
+
+# Paste your ssh public key into your github account settings.
+
+# Then run the below command
+# ssh -T git@github.com
+
+# If it says something like the following, it worked:
+
+# "Hi username! You've successfully authenticated, but Github does
+# not provide shell access."
